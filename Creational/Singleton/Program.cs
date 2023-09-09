466067b24e2ef8ac1d4183a57dd2acb9fe989469
omarsorour123 <90939373+omarsorour123@users.noreply.github.com>
2023-09-09 19:10:50 +0300

    Add files via upload

---
+++ Creational/Singleton/Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{   public sealed class Logger
+    {
+        private static Logger instance;
+        private string logFileName="logs.log";
+        private StreamWriter logWriter;
+     private Logger()
+        {
+            logWriter = new StreamWriter(logFileName, true);
+        }
+
+        public static Logger Instance()
+        {  
+             if (instance == null)
+            {
+                instance = new Logger();
+            }
+             return instance;
+
+        }
+        public void Log(string message)
+        {
+            string formattedMessage = $"{DateTime.Now}: {message}";
+            logWriter.WriteLine(formattedMessage);
+            logWriter.Flush();
+
+        }
+        public void Close()
+        {
+            logWriter.Close();
+        }
+
+    }
+
+    private static void Main(string[] args)
+    {
+        Logger logger1= Logger.Instance();
+        Logger logger2= Logger.Instance();
+        if(logger1 == logger2) {
+            Console.WriteLine("equal");
+        }
+        logger1.Log("hi");
+        logger1.Close();
+    }
+}
